8d1c292eb58ddd8ff2411d46a06255898175653e
billydean <billy.goehring@gmail.com>
2022-03-28 14:25:07 -0700

    finished mvp tasks

--- src/components/tests/Display.test.js
+++ src/components/tests/Display.test.js
@@ -2,9 +2,39 @@ import React from 'react';
 import { render, fireEvent, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import Display from './../Display';
+import userEvent from '@testing-library/user-event';
+import mockFetchShow from './../../api/fetchShow';
+jest.mock('./../../api/fetchShow');
 
-test('renders without errors with no props', async () => { });
+const fakeData = {
+	name: 'Stranger Things',
+	summary: 'A show about strang things',
+	seasons: [
+		{id: 0, name: 'Season 1', episodes: []},
+		{id: 1, name: 'Season 2', episodes: []}
+	]
+}
 
-test('renders Show component when the button is clicked ', () => { });
+test('renders without errors with no props', async () => { 
+    render(<Display />)
+ });
 
-test('renders show season options matching your data when the button is clicked', () => { });
+test('renders Show component when the button is clicked ', async () => {
+    mockFetchShow.mockResolvedValueOnce(fakeData)
+    render(<Display />)
+    const button = screen.getByRole('button');
+    
+    userEvent.click(button);
+    const show = await screen.findByTestId('show-container');
+    expect(show).toBeInTheDocument();
+ });
+
+test('renders show season options matching your data when the button is clicked', async () =>{ 
+    mockFetchShow.mockResolvedValueOnce(fakeData)
+    render(<Display/>);
+    const button = screen.getByRole('button');
+    userEvent.click(button);
+        const seasonOptions = await screen.findAllByTestId('season-option');
+        expect(seasonOptions).toHaveLength(2);
+    
+});
--- src/components/tests/Show.test.js
+++ src/components/tests/Show.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import { render, fireEvent, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import Show from './../Show';
+import userEvent from '@testing-library/user-event';
 
-Build an example data structure that contains the show data in the correct format. A show should contain a name, a summary and an array of seasons, each with a id, name and an (empty) list of episodes within them. Use console.logs within the client code if you need to to verify the structure of show data.
 const fakeData = {
 	name: 'Stranger Things',
 	summary: 'A show about strang things',
@@ -14,18 +14,42 @@ const fakeData = {
 }
 
 test('renders without errors', () => { 
-	render( <Show show={{name: 'Stranger Things',
-	summary: 'A show about strang things',
-	seasons: [
-		{id: 0, name: 'Season 1', episodes: []},
-		{id: 1, name: 'Season 2', episodes: []}
-		]}} selectedSeason={'none'} handleSelect={Function.prototype} /> )
+	render( <Show show={fakeData} selectedSeason={'none'} handleSelect={Function.prototype} /> )
 });
 
-test('renders Loading component when prop show is null', () => { });
+test('renders Loading component when prop show is null', () => {  
+	render( <Show show={null} selectedSeason={'none'} handleSelect={Function.prototype}/> )
+	const loading = screen.getByTestId(/loading-container/i);
+	expect(loading).toBeTruthy();
+	expect(loading).toBeInTheDocument();
+	expect(loading).toBeVisible();
 
-test('renders same number of options seasons are passed in', () => { });
+});
 
-test('handleSelect is called when an season is selected', () => { });
+test('renders same number of options seasons are passed in', () => { 
+	render( <Show show={fakeData} selectedSeason={'none'} handleSelect={Function.prototype} /> )
+	const seasons = screen.getAllByTestId('season-option');
+	expect(seasons).toHaveLength(fakeData.seasons.length);
+});
 
-test('component renders when no seasons are selected and when rerenders with a season passed in', () => { });
+
+
+test('handleSelect is called when an season is selected', async () => { 
+	const handleSelect = jest.fn();
+	render( <Show show={fakeData} selectedSeason={'none'} handleSelect={handleSelect} /> )
+	// const selectButton = screen.getByLabelText(/season 1/i);
+	const select = screen.getByLabelText(/Select A Season/i);
+	userEvent.selectOptions(select, ['0']);
+	expect(handleSelect).toBeCalled();
+	// userEvent.click(selectButton);
+	// expect(handleSelect).toHaveBeenCalled();
+});
+
+test('component renders when no seasons are selected and when rerenders with a season passed in', () => { 
+	const { rerender } = render(<Show show={fakeData} selectedSeason={'none'} handleSelect={Function.prototype} />);
+	let episodes = screen.queryByTestId('episodes-container');
+	expect(episodes).not.toBeInTheDocument();
+	rerender(<Show show={fakeData} selectedSeason={1} handleSelect={Function.prototype} />)
+	episodes = screen.queryByTestId('episodes-container');
+	expect(episodes).toBeInTheDocument();
+});
